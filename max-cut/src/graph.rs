eafb3512135b6f85c2b62e79a7e23b5b0ee1fa58
Jakob Sanowski <jakob.sanowski@uni-konstanz.de>
2023-03-12 16:34:29 +0100

    change \'Graph\' to use adjacency-lists

--- max-cut/src/graph.rs
+++ max-cut/src/graph.rs
@@ -22,7 +22,8 @@ impl Debug for Edge {
 /// For simplicity, once created, only edges can be added or removed.
 #[derive(PartialEq, Debug)]
 pub struct Graph {
-    adjacency_matrix: Vec<Vec<bool>>,
+    // adjacency_matrix: Vec<Vec<bool>>,
+    adjacency_lists: Vec<Vec<usize>>,
 }
 
 #[derive(Debug)]
@@ -41,7 +42,8 @@ impl fmt::Display for NotQuadError {
 impl Graph {
     pub fn new_empty(n: usize) -> Graph {
         Graph {
-            adjacency_matrix: vec![vec![false; n]; n],
+            // adjacency_matrix: vec![vec![false; n]; n],
+            adjacency_lists: vec![vec![]; n],
         }
     }
 
@@ -55,13 +57,33 @@ impl Graph {
             return Err(NotQuadError);
         }
 
+        let mut adjacency_lists = vec![vec![]; data.len()];
+
+        for row in 0..data.len() {
+            for i in 0..data[row].len() {
+                if data[row][i] {
+                    if !adjacency_lists[row].contains(&i) {
+                        adjacency_lists[row].push(i)
+                    }
+                    if !adjacency_lists[i].contains(&row) {
+                        adjacency_lists[i].push(row)
+                    }
+                };
+            }
+        }
+
         Ok(Graph {
-            adjacency_matrix: data,
+            // adjacency_matrix: data,
+            adjacency_lists,
         })
     }
 
     pub fn add_edge(&mut self, edge: &(usize, usize)) {
-        self.adjacency_matrix[edge.0][edge.1] = true;
+        // self.adjacency_matrix[edge.0][edge.1] = true;
+        if !self.adjacency_lists[edge.0].contains(&edge.1) {
+            self.adjacency_lists[edge.0].push(edge.1);
+            self.adjacency_lists[edge.1].push(edge.0);
+        }
     }
 
     pub fn add_edges(&mut self, edges: &[(usize, usize)]) {
@@ -71,88 +93,70 @@ impl Graph {
     }
 
     pub fn contains_edge(&self, edge: &(usize, usize)) -> bool {
-        self.adjacency_matrix[edge.0][edge.1]
+        // self.adjacency_matrix[edge.0][edge.1]
+        self.adjacency_lists[edge.0].contains(&edge.1)
     }
 
     pub fn all_edges(&self) -> Vec<Edge> {
         // Filter out all 'false' entries
-        self.adjacency_matrix
-            .iter()
-            .scan(0usize, |i, r| {
-                let old = *i;
-                *i += 1;
-                Some(
-                    r.iter()
-                        .scan((old, 0usize), |j, &e| {
-                            let edge = Edge(j.0, j.1);
-                            j.1 += 1;
-                            if e {
-                                Some(Some(edge))
-                            } else {
-                                Some(None)
-                            }
-                        })
-                        .flatten(),
-                )
-            })
-            .flatten()
-            .collect()
+        // self.adjacency_matrix
+        //     .iter()
+        //     .scan(0usize, |i, r| {
+        //         let old = *i;
+        //         *i += 1;
+        //         Some(
+        //             r.iter()
+        //                 .scan((old, 0usize), |j, &e| {
+        //                     let edge = Edge(j.0, j.1);
+        //                     j.1 += 1;
+        //                     if e {
+        //                         Some(Some(edge))
+        //                     } else {
+        //                         Some(None)
+        //                     }
+        //                 })
+        //                 .flatten(),
+        //         )
+        //     })
+        //     .flatten()
+        //     .collect()
+
+        let mut edges = vec![];
+
+        for i in 0..self.adjacency_lists.len() {
+            self.adjacency_lists[i].iter().for_each(|x| {
+                if *x > i {
+                    edges.push(Edge(i, *x))
+                }
+            });
+        }
+
+        edges
     }
 
     pub fn edge_size(&self) -> usize {
-        self.adjacency_matrix.iter().flatten().fold(0, |mut n, x| {
-            if *x {
-                n += 1
-            };
-            n
-        })
+        self.adjacency_lists
+            .iter()
+            .fold((0usize, 0usize), |mut n, x| {
+                n.0 += x.iter().fold(0, |mut m, z| {
+                    if *z > n.1 {
+                        m += 1
+                    }
+
+                    m
+                });
+
+                n
+            })
+            .0
     }
 
     pub fn size(&self) -> usize {
-        self.adjacency_matrix.len()
+        self.adjacency_lists.len()
     }
 
     pub fn get_neighbors(&self, vertex: usize) -> Vec<usize> {
-        self.adjacency_matrix[vertex]
-            .iter()
-            .scan(0usize, |i, e| {
-                let index = *i;
-                *i += 1;
-                match e {
-                    true => Some(Some(index)),
-                    false => Some(None),
-                }
-            })
-            .flatten()
-            .collect()
-    }
-
-    pub fn get_neighbors_undireted(&self, vertex: usize) -> Vec<usize> {
-        let mut neighbors = self.adjacency_matrix
-            .iter()
-            .scan(0usize, |i, e| {
-                let index = *i;
-                *i += 1;
-                Some(
-                    e.iter()
-                        .scan((index, 0usize), |i, e| {
-                            let index = *i;
-                            i.1 += 1;
-                            // dbg!(index,e);
-                            match (index, e) {
-                                ((v, _), true) if v == vertex => Some(Some(index.1)),
-                                ((_, v), true) if v == vertex => Some(Some(index.0)),
-                                _ => Some(None),
-                            }
-                        })
-                        .flatten(),
-                )
-            })
-            .flatten()
-            .collect::<Vec<usize>>();
-        neighbors.sort_unstable();
-        neighbors.dedup();
-        neighbors
+        self.adjacency_lists[vertex].clone()
     }
 }
 
@@ -173,10 +177,9 @@ mod tests {
         let expected = vec![
             Edge(0usize, 1usize),
             Edge(0usize, 2usize),
+            Edge(0usize, 3usize),
             Edge(1usize, 2usize),
             Edge(1usize, 3usize),
-            Edge(2usize, 0usize),
-            Edge(3usize, 0usize),
         ];
 
         assert_eq!(graph.all_edges(), expected);
@@ -192,21 +195,7 @@ mod tests {
         ])
         .unwrap();
 
-        assert_eq!(graph.get_neighbors(0usize), vec![1usize, 2usize]);
-        assert_eq!(graph.get_neighbors(2usize), vec![0usize])
-    }
-
-    #[test]
-    fn get_neighbors_undireted() {
-        let graph = Graph::new(vec![
-            vec![false, true, true, false],
-            vec![false, false, true, true],
-            vec![true, false, false, false],
-            vec![true, false, false, false],
-        ])
-        .unwrap();
-
-        assert_eq!(graph.get_neighbors_undireted(0usize), vec![1usize, 2usize, 3usize]);
-        assert_eq!(graph.get_neighbors_undireted(2usize), vec![0usize, 1usize])
+        assert_eq!(graph.get_neighbors(0usize), vec![1usize, 2usize, 3usize]);
+        assert_eq!(graph.get_neighbors(2usize), vec![0usize, 1usize])
     }
 }
